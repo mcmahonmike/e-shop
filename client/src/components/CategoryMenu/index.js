f79a25473a3e78cad078656e020378da2af6e452
Hephaestus <mcmahonmike9@yahoo.com>
2023-01-03 08:53:58 -0600

    cart UI functionality implemented

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -15,7 +15,9 @@ function CategoryMenu() {
   const { data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
     if (categoryData) {
+      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories,
