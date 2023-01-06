c5a5880ce8ce3de45412a40281aba8fb361e37af
Hephaestus <mcmahonmike9@yahoo.com>
2023-01-06 13:14:35 -0600

    updated resolvers and typeDefs, still issues with stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,11 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
+
     categories: async () => {
       return await Category.find();
     },
@@ -50,8 +52,45 @@ const resolvers = {
       }
 
       throw new AuthenticationError('Not logged in');
+    },
+    checkout: async (parent, args, context) => {
+      const order = new Order({ products: args.products });
+      const { products } = await order.populate('product');
+      const line_items = [];
+    
+        for (let i = 0; i < products.length; i++) {
+          // generate product id
+          const product = await stripe.products.create({
+            name: products[i].name,
+            description: products[i].description
+          });
+
+          // generate price id using the product id
+          const price = await stripe.prices.create({
+            product: product.id,
+            unit_amount: products[i].price * 100,
+            currency: 'usd',
+          });
+
+          // add price id to the line items array
+          line_items.push({
+            price: price.id,
+            quantity: 1
+          });
+        }
+        const session = await stripe.checkout.sessions.create({
+          payment_method_types: ['card'],
+          line_items,
+          mode: 'payment',
+          success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+          cancel_url: 'https://example.com/cancel'
+        });
+      
+        return { session: session.id };
     }
-  },
+    },
+            
+
   Mutation: {
     addUser: async (parent, args) => {
       const user = await User.create(args);
@@ -104,3 +143,4 @@ const resolvers = {
 };
 
 module.exports = resolvers;
+
